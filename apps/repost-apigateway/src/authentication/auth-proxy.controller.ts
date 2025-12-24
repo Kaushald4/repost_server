d7c0beb79b2db58bce9fc6d5e218b174d0a0054c
Kaushal <kaushalkumar304@gmail.com>
2025-12-24 20:49:06 +0530

    feat: enhance cookie handling in login method to use configurable values for expiration and options

--- apps/repost-apigateway/src/authentication/auth-proxy.controller.ts
+++ apps/repost-apigateway/src/authentication/auth-proxy.controller.ts
@@ -19,6 +19,7 @@ import type { Request, Response } from 'express';
 import { Public } from '@app/common';
 import type { ClientGrpc } from '@nestjs/microservices';
 import { Observable, firstValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 
 interface AuthService {
   register(data: RegisterRequest): Observable<any>;
@@ -31,7 +32,10 @@ interface AuthService {
 export class AuthenticationProxyController implements OnModuleInit {
   private svc: AuthService;
 
-  constructor(@Inject('AUTH_SERVICE') private client: ClientGrpc) {}
+  constructor(
+    @Inject('AUTH_SERVICE') private client: ClientGrpc,
+    private configService: ConfigService,
+  ) {}
   onModuleInit() {
     this.svc = this.client.getService<AuthService>('AuthService');
   }
@@ -56,24 +60,38 @@ export class AuthenticationProxyController implements OnModuleInit {
         userAgent: req.headers['user-agent'] || '',
       }),
     );
+
+    const cookieDomain = this.configService.get<string>('COOKIE_DOMAIN')!;
+    const cookieHttpOnly = this.configService.get<string>('COOKIE_HTTP_ONLY')!;
+    const cookiePath = this.configService.get<string>('COOKIE_PATH')!;
+    const cookieSecure = this.configService.get<string>('COOKIE_SECURE')!;
+    const cookieSameSite = this.configService.get<string>('COOKIE_SAME_SITE')!;
+
+    const cookieExpirationTime = this.configService.get<string>(
+      'COOKIE_EXPIRATION_TIME',
+    )! as unknown as number;
+    const refreshCookieExpirationTime = this.configService.get<string>(
+      'REFRESH_COOKIE_EXPIRATION_TIME',
+    )! as unknown as number;
+
     const options: Record<string, any> = {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none' as const,
-      path: '/',
-      domain: undefined,
+      httpOnly: cookieHttpOnly === 'true',
+      secure: cookieSecure === 'true',
+      sameSite: cookieSameSite as 'lax' | 'strict' | 'none',
+      path: cookiePath,
+      domain: cookieDomain,
     };
     res.cookie('access_token', response.accessToken, {
       ...options,
-      maxAge: 20,
+      maxAge: Number(cookieExpirationTime) * 1000,
     });
     res.cookie('refresh_token', response.refreshToken, {
       ...options,
-      maxAge: 7 * 24 * 60 * 60,
+      maxAge: Number(refreshCookieExpirationTime) * 1000,
     });
     res.cookie('refresh_token_id', response.refreshTokenId, {
       ...options,
-      maxAge: 7 * 24 * 60 * 60,
+      maxAge: Number(refreshCookieExpirationTime) * 1000,
     });
     return res.json({
       userId: response.userId,
